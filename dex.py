86965cbbe7012948b046cb13541d7dccdfdedf52
Ryan Lee <rjlguitar@gmail.com>
2020-12-05 11:56:52 -0800

    added loop for running the program, line comments

--- dex.py
+++ dex.py
@@ -1,12 +1,15 @@
+#import methods 
 from pokemon import Pokemon
 from moves import Move
 from trainer import Trainer
 
 class Pokedex:
+    #initial greeting for user
     def greet(self):
         print('Hello! Welcome to the Pokedex.')
 
-    def create(self):
+    #create entry for pokedex with user input 
+    def create_entry(self):
         trainer_name = input('What is your name? ')
         trainer_age = input('How old are you? ')
         trainer_city = input('What city are you from? ')
@@ -18,13 +21,28 @@ class Pokedex:
         pokemon.add_moves(pokemon_move)
         print('Thank you for your information!')
         print('Here is your entry: ')
+        #creates list of trainer attributes
         trainer_list = ['Name: ' + trainer_name, 'Age: ' + trainer_age, 'City: ' + trainer_city]
         for i in trainer_list:
             print(i)
+        #creates list for pokemon attributes
         pokemon_list = ['Pokemon: ' + pokemon_name, 'Type: ' + pokemon_type, 'Signature Move: ' + pokemon_move]
         for i in pokemon_list:
             print(i)
 
-dex1 = Pokedex()
-dex1.greet()
-dex1.create()
+#create loop for repeating entries
+if __name__ == "__main__":
+    dex_running = True
+
+    dex = Pokedex()
+    dex.greet()
+    dex.create_entry()
+
+    while dex_running:
+        repeat = input('Add another entry? Y or N: ')
+        
+        if repeat.lower() == 'n':
+            print('Thanks for using the PokeDex!')
+            dex_running = False
+        else:
+            dex.create_entry()
