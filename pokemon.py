dd05cf6346e49974ab462d0d0f48df97da27dc3c
Ryan Lee <rjlguitar@gmail.com>
2020-12-05 11:32:47 -0800

    fixed typo error in import

--- pokemon.py
+++ pokemon.py
@@ -1,16 +1,10 @@
-from moves import Moves
+from moves import Move
 
 class Pokemon:
     def __init__(self, name, type):
         self.name = name
         self.type = type
         self.moves = list()
-    
-    def intro(self):
-        print(f'{self.name} is a {self.type} type.')
-        print(f"{self.name}'s moves are: ")
-        for i in self.moves:
-            print(i)
     
     def add_moves(self, move):
         self.moves.append(move)
